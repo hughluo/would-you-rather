f34019a4025d4ecb37c3ce0fa7dcaf7f2f5ca5aa
wexort <bugwriter@outlook.com>
2021-02-14 19:57:14 +0100

    add avatar urls

--- src/_DATA.js
+++ src/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: ,
+    avatarURL: 'https://gravatar.com/avatar/ff149c5e70ae12dea893526112da679e?s=400&d=robohash&r=x',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: ,
+    avatarURL: 'https://gravatar.com/avatar/76eed958df22d9823a76b025b9bda80c?s=400&d=robohash&r=x',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: ,
+    avatarURL: 'https://gravatar.com/avatar/16c376d79dab9de7a9ce77b0816c82b8?s=400&d=robohash&r=x',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
