bf5405741d67b6deebd895c96dea977165068fc2
wexort <bugwriter@outlook.com>
2021-02-22 19:39:21 +0100

    use className instead of class

--- src/components/ToggleQuestionList.js
+++ src/components/ToggleQuestionList.js
@@ -3,20 +3,20 @@ function ToggleQuestionList({ toggleValue, setToggleValue }) {
 	const ANSWERED = 'ANSWERED';
 
 	return (
-		<div class="tabs is-toggle is-fullwidth">
+		<div className="tabs is-toggle is-fullwidth">
 			<ul>
-				<li class={toggleValue === ANSWERED ? 'is-active' : ''}>
+				<li className={toggleValue === ANSWERED ? 'is-active' : ''}>
 					<a onClick={() => setToggleValue(ANSWERED)}>
-						<span class="icon is-small">
-							<i class="fas fa-image" aria-hidden="true" />
+						<span className="icon is-small">
+							<i className="fas fa-image" aria-hidden="true" />
 						</span>
 						<span>Answered</span>
 					</a>
 				</li>
-				<li class={toggleValue === UNANSWERED ? 'is-active' : ''}>
+				<li className={toggleValue === UNANSWERED ? 'is-active' : ''}>
 					<a onClick={() => setToggleValue(UNANSWERED)}>
-						<span class="icon is-small">
-							<i class="fas fa-music" aria-hidden="true" />
+						<span className="icon is-small">
+							<i className="fas fa-music" aria-hidden="true" />
 						</span>
 						<span>Unanswered</span>
 					</a>
