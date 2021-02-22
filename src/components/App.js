31b0769013595788fe978b5db899d04ff2f2251f
wexort <bugwriter@outlook.com>
2021-02-22 20:42:01 +0100

    use Navbar, Tab

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,8 @@ import { handleInitialData } from '../actions/shared';
 
 import QuestionList from './QuestionList';
 import QuestionPoll from './QuestionPoll';
-import Nav from './Nav';
+import Navbar from './Navbar';
+import Tab from './Tab';
 
 class App extends Component {
 	componentDidMount() {
@@ -17,13 +18,16 @@ class App extends Component {
 		return (
 			<Router>
 				<Fragment>
-					<LoadingBar />
-					<Nav />
-					<div>
-						<Switch>
-							<Route path={'/'} exact component={QuestionList} />
-							<Route path={'/questions/:question_id'} component={QuestionPoll} />
-						</Switch>
+					<div className="container">
+						<LoadingBar />
+						<Navbar />
+						<Tab />
+						<div>
+							<Switch>
+								<Route path={'/'} exact component={QuestionList} />
+								<Route path={'/questions/:question_id'} component={QuestionPoll} />
+							</Switch>
+						</div>
 					</div>
 				</Fragment>
 			</Router>
