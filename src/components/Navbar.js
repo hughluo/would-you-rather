09aa97d5832be1ba72d36f2739c5218f86f13e87
wexort <bugwriter@outlook.com>
2021-02-22 21:54:56 +0100

    remove needless link in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,13 @@ function Navbar() {
 	return (
 		<div>
 			<nav className="navbar" role="navigation" aria-label="main navigation">
-				<div id="navbarBasicExample" className="navbar-menu">
-					<div className="navbar-start">
-						<a className="navbar-item">Home</a>
-
-						<a className="navbar-item">Documentation</a>
-					</div>
-				</div>
-
 				<div className="navbar-end">
 					<div className="navbar-item">
 						<div className="buttons">
-							<a className="button is-primary">
+							<a className="button nes-btn is-primary">
 								<strong>Sign up</strong>
 							</a>
-							<a className="button is-light">Log in</a>
+							<a className="button nes-btn is-light">Log in</a>
 						</div>
 					</div>
 				</div>
