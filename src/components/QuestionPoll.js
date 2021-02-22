44caec8ab605df44f681a7a3b0e71a57dc3fe1a4
wexort <bugwriter@outlook.com>
2021-02-22 23:34:50 +0100

    update QuestionPoll radios as controlled component, update layout

--- src/components/QuestionPoll.js
+++ src/components/QuestionPoll.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
@@ -6,6 +6,9 @@ import Loading from './Loading';
 
 function QuestionPoll({ users, questions, loading }) {
 	const { question_id } = useParams();
+	const OPTION_ONE = 'optionOne';
+	const OPTION_TWO = 'optionTwo';
+	const [ radioValue, setRadioValue ] = useState(OPTION_ONE);
 
 	if (loading) {
 		return <Loading />;
@@ -22,21 +25,51 @@ function QuestionPoll({ users, questions, loading }) {
 	console.log(`question: ${question}`);
 	const author = users[question['author']];
 
+	// const handleRadioChange = (e) => {
+	// 	setRadioValue(e.target.value);
+	// };
+
 	return (
-		<div>
-			<h1>QuestionPoll</h1>
-			<div className="avatar">
-				<img src={author.avatarURL} alt="avatar" />
-			</div>
-			<h2> {author['name']} asks: Would you rather...</h2>
+		<div className="card" style={{ padding: '20px' }}>
 			<div>
-				<input type="radio" name="option" value="optionOne" />
-				<label>{question['optionOne']['text']}</label>
+				<div className="avatar">
+					<img src={author.avatarURL} alt="avatar" width="256" height="256" />
+				</div>
+				<div style={{ padding: '20px' }}>
+					<p className="nes-balloon from-left nes-pointer" style={{ fontSize: '125%' }}>
+						@{author['name']}: Would you rather...
+					</p>
+				</div>
 			</div>
 
 			<div>
-				<input type="radio" name="option" value="optionTwo" />
-				<label>{question['optionTwo']['text']}</label>
+				<div style={{ fontSize: '150%' }}>
+					<label>
+						<input
+							type="radio"
+							className="nes-radio"
+							name="answer"
+							checked={radioValue === OPTION_ONE}
+							onChange={() => setRadioValue(OPTION_ONE)}
+						/>
+						<span>{question[OPTION_ONE]['text']}</span>
+					</label>
+
+					<br />
+					<label>
+						<input
+							type="radio"
+							className="nes-radio"
+							name="answer"
+							checked={radioValue === OPTION_TWO}
+							onChange={() => setRadioValue(OPTION_TWO)}
+						/>
+						<span>{question[OPTION_TWO]['text']}</span>
+					</label>
+				</div>
+			</div>
+			<div className="container">
+				<button className="nes-btn is-success">Submit</button>
 			</div>
 		</div>
 	);
