f3acf516db3443a8c5ecd6ea7b614eaaa862b85c
wexort <bugwriter@outlook.com>
2021-02-22 19:32:29 +0100

    remove boostrap, use bulma for ToggleQuestionList

--- src/components/ToggleQuestionList.js
+++ src/components/ToggleQuestionList.js
@@ -1,34 +1,28 @@
-import { ToggleButton, ButtonGroup } from 'react-bootstrap';
-
 function ToggleQuestionList({ toggleValue, setToggleValue }) {
 	const UNANSWERED = 'UNANSWERED';
 	const ANSWERED = 'ANSWERED';
 
 	return (
-		<ButtonGroup toggle>
-			<ToggleButton
-				key="0"
-				type="radio"
-				variant={toggleValue === UNANSWERED ? 'primary' : 'light'}
-				name="radio"
-				value={UNANSWERED}
-				checked={toggleValue === UNANSWERED}
-				onChange={(e) => setToggleValue(e.currentTarget.value)}
-			>
-				Unanswered Questions
-			</ToggleButton>
-			<ToggleButton
-				key="1"
-				type="radio"
-				variant={toggleValue === ANSWERED ? 'primary' : 'light'}
-				name="radio"
-				value={ANSWERED}
-				checked={toggleValue === ANSWERED}
-				onChange={(e) => setToggleValue(e.currentTarget.value)}
-			>
-				Answered Questions
-			</ToggleButton>
-		</ButtonGroup>
+		<div class="tabs is-toggle is-fullwidth">
+			<ul>
+				<li class={toggleValue === ANSWERED ? 'is-active' : ''}>
+					<a onClick={() => setToggleValue(ANSWERED)}>
+						<span class="icon is-small">
+							<i class="fas fa-image" aria-hidden="true" />
+						</span>
+						<span>Answered</span>
+					</a>
+				</li>
+				<li class={toggleValue === UNANSWERED ? 'is-active' : ''}>
+					<a onClick={() => setToggleValue(UNANSWERED)}>
+						<span class="icon is-small">
+							<i class="fas fa-music" aria-hidden="true" />
+						</span>
+						<span>Unanswered</span>
+					</a>
+				</li>
+			</ul>
+		</div>
 	);
 }
 
