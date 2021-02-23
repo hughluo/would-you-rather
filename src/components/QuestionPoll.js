656c86007fcf896b0952a910a755299afa898a9b
wexort <bugwriter@outlook.com>
2021-02-23 22:41:51 +0100

    impl QuestionPoll

--- src/components/QuestionPoll.js
+++ src/components/QuestionPoll.js
@@ -1,5 +1,35 @@
-function QuestionPoll() {
-	return <div>This is Question Poll.</div>;
+import QuestionBalloon from './QuestionBalloon';
+
+function QuestionPoll({ author, question }) {
+	const optionOneVotesNum = question.optionOne.votes.length;
+	const optionTwoVotesNum = question.optionTwo.votes.length;
+	const optionOnePercentage = optionOneVotesNum / (optionOneVotesNum + optionTwoVotesNum) * 100;
+	const optionTwoPercentage = optionTwoVotesNum / (optionOneVotesNum + optionTwoVotesNum) * 100;
+
+	return (
+		<div>
+			<div className="nes-container with-title">
+				<p className="title">Question Poll</p>
+				<QuestionBalloon author={author} />
+				<br />
+				<div className="nes-container with-title">
+					<p className="title">Result</p>
+					<div>
+						<span>
+							{optionOnePercentage}% Folks choose to {question.optionOne.text}
+						</span>
+						<progress className="nes-progress is-primary" value={optionOnePercentage} max="100" />
+					</div>
+					<div>
+						<span>
+							{optionTwoPercentage}% Folks choose to {question.optionTwo.text}
+						</span>
+						<progress className="nes-progress is-primary" value={optionTwoPercentage} max="100" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default QuestionPoll;
