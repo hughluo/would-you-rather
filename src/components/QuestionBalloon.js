e7ec0044b3fc4b2967b24c54dd98e2dd80128d4c
wexort <bugwriter@outlook.com>
2021-02-23 22:41:26 +0100

    use columns for QuestionBalloon

--- src/components/QuestionBalloon.js
+++ src/components/QuestionBalloon.js
@@ -1,13 +1,19 @@
 function QuestionBalloon({ author }) {
 	return (
 		<div>
-			<div className="avatar">
-				<img src={author.avatarURL} alt="avatar" width="256" height="256" />
-			</div>
-			<div style={{ padding: '20px' }}>
-				<p className="nes-balloon from-left nes-pointer" style={{ fontSize: '125%' }}>
-					@{author.name}: Would you rather...
-				</p>
+			<div className="columns is-gapless">
+				<div className="column is-one-quarter">
+					<div className="avatar">
+						<img src={author.avatarURL} alt="avatar" width="256" height="256" />
+					</div>
+				</div>
+				<div className="column is-one-quarter ">
+					<div style={{ padding: '20px' }}>
+						<p className="nes-balloon from-left nes-pointer" style={{ fontSize: '125%' }}>
+							@{author.name}: Would you rather...
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
