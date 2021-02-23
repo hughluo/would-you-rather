8220e9a9bbfb77fdd084b421cb26d4bc670943aa
wexort <bugwriter@outlook.com>
2021-02-23 22:41:00 +0100

    swap postion, let unanswered button be in the left side

--- src/components/ToggleQuestionList.js
+++ src/components/ToggleQuestionList.js
@@ -2,20 +2,20 @@ function ToggleQuestionList({ showAnswered, setShowAnswered }) {
 	return (
 		<div className="tabs is-toggle is-fullwidth">
 			<ul>
-				<li className={showAnswered ? 'is-active' : ''}>
-					<a onClick={() => setShowAnswered(true)}>
-						<span className="icon is-small">
-							<i className="fas fa-image" aria-hidden="true" />
-						</span>
-						<span>Answered</span>
-					</a>
-				</li>
 				<li className={showAnswered ? '' : 'is-active'}>
 					<a onClick={() => setShowAnswered(false)}>
 						<span className="icon is-small">
 							<i className="fas fa-music" aria-hidden="true" />
 						</span>
 						<span>Unanswered</span>
+					</a>
+				</li>
+				<li className={showAnswered ? 'is-active' : ''}>
+					<a onClick={() => setShowAnswered(true)}>
+						<span className="icon is-small">
+							<i className="fas fa-image" aria-hidden="true" />
+						</span>
+						<span>Answered</span>
 					</a>
 				</li>
 			</ul>
