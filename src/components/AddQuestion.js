1d24a00860fd869e21dd74aa0e8cae9584a22b90
wexort <bugwriter@outlook.com>
2021-02-26 21:06:23 +0100

    upgrade AddQuestion to container

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,5 +1,18 @@
-function AddQuestion() {
+import { connect } from 'react-redux';
+
+import Loading from './Loading';
+
+function AddQuestion({ loading, users }) {
+	if (loading) {
+		return <Loading />;
+	}
 	return <div>AddQuestion</div>;
 }
 
-export default AddQuestion;
+function mapStateToProps({ authedUser, users }) {
+	return {
+		loading: authedUser === null
+	};
+}
+
+export default connect(mapStateToProps)(AddQuestion);
