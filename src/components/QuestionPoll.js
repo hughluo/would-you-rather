b9b977d371923c8550243b01c8878c8a5d09254f
wexort <bugwriter@outlook.com>
2021-02-27 09:08:57 +0100

    update QuestionPoll to let the option voted by authedUser clearly marked

--- src/components/QuestionPoll.js
+++ src/components/QuestionPoll.js
@@ -5,7 +5,7 @@ function QuestionPoll({ author, question, authedUser }) {
 	const optionTwoVotesNum = question.optionTwo.votes.length;
 	const optionOnePercentage = optionOneVotesNum / (optionOneVotesNum + optionTwoVotesNum) * 100;
 	const optionTwoPercentage = optionTwoVotesNum / (optionOneVotesNum + optionTwoVotesNum) * 100;
-	const authedUserVoteForOptionOne = authedUser in question.optionOne.votes;
+	const authedUserVoteForOptionOne = question.optionOne.votes.includes(authedUser);
 
 	return (
 		<div>
@@ -16,16 +16,16 @@ function QuestionPoll({ author, question, authedUser }) {
 				<div className="nes-container with-title">
 					<p className="title">Result</p>
 					<div>
-						<span>
-							{authedUserVoteForOptionOne ? 'You and other ' : ''}
-							{optionOnePercentage.toFixed(2)}% Folks choose to {question.optionOne.text}
+						<span style={{ color: authedUserVoteForOptionOne ? 'red' : 'black' }}>
+							{optionOnePercentage.toFixed(2)}% folks choose to {question.optionOne.text}
+							{authedUserVoteForOptionOne ? ' (You voted for this)' : ''}
 						</span>
 						<progress className="nes-progress is-primary" value={optionOnePercentage} max="100" />
 					</div>
 					<div>
-						<span>
-							{authedUserVoteForOptionOne ? '' : 'You and other '}
-							{optionTwoPercentage.toFixed(2)}% Folks choose to {question.optionTwo.text}
+						<span style={{ color: authedUserVoteForOptionOne ? 'black' : 'red' }}>
+							{optionTwoPercentage.toFixed(2)}% folks choose to {question.optionTwo.text}
+							{authedUserVoteForOptionOne ? '' : ' (You voted for this)'}
 						</span>
 						<progress className="nes-progress is-primary" value={optionTwoPercentage} max="100" />
 					</div>
