0cb00b2c03a3541b897eb4b4e59e5d3723aacd3b
wexort <bugwriter@outlook.com>
2021-02-27 09:43:55 +0100

    update style in QuestionPoll

--- src/components/QuestionPoll.js
+++ src/components/QuestionPoll.js
@@ -16,14 +16,14 @@ function QuestionPoll({ author, question, authedUser }) {
 				<div className="nes-container with-title">
 					<p className="title">Result</p>
 					<div>
-						<span style={{ color: authedUserVoteForOptionOne ? 'red' : 'black' }}>
+						<span className={authedUserVoteForOptionOne ? 'nes-text is-primary' : ''}>
 							{optionOnePercentage.toFixed(2)}% folks choose to {question.optionOne.text}
 							{authedUserVoteForOptionOne ? ' (You voted for this)' : ''}
 						</span>
 						<progress className="nes-progress is-primary" value={optionOnePercentage} max="100" />
 					</div>
 					<div>
-						<span style={{ color: authedUserVoteForOptionOne ? 'black' : 'red' }}>
+						<span className={authedUserVoteForOptionOne ? '' : 'nes-text is-primary'}>
 							{optionTwoPercentage.toFixed(2)}% folks choose to {question.optionTwo.text}
 							{authedUserVoteForOptionOne ? '' : ' (You voted for this)'}
 						</span>
